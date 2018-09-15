8efc2a15be630254bd691146efea54aedbac8be8
baron517 <450175465@qq.com>
2018-09-15 14:37:39 +0800

    a

--- loan/js/app.js
+++ loan/js/app.js
@@ -15,10 +15,49 @@ $(function()
     hideLoading:function()
     {
         $("#ShowLoading").remove();
+    },
+    error500:function()
+    {
+    	$(".mui-content").html('<div class="error-col"><img src="../images/error/500.png"/><button type="button" class="mui-btn mui-btn-outlined" class="go-home">返回首页</button></div>');
+	
+    },
+    error404:function()
+    {
+    	$(".mui-content").html('<div class="error-col"><img src="../images/error/404.png"/><button type="button" class="mui-btn mui-btn-outlined" class="go-home">返回首页</button></div>');
+	
+    },
+     errorNet:function()
+    {
+    	$(".mui-content").html('<div class="error-col"><img src="../images/error/wangluo.png"/><button type="button" class="mui-btn mui-btn-outlined" class="go-home">返回首页</button></div>');
+	
+    },
+    errorDetail:function()
+    {
+    	$(".mui-content").html('<div class="error-col"><img src="../images/error/tixian.png"/></div>');
+	
+    },
+    errorNews:function()
+    {
+    	$(".mui-content").html('<div class="error-col"><img src="../images/error/xiaoxi.png"/></div>');
+	
     }
+    
+    
+    
 }
 
 
+$(".mui-content").on("click",".go-home",function()
+{
+	mui.openWindow({
+      			url: 'home.html',
+      			id: 'home.html.html',
+			    waiting:{
+			      autoShow:false
+			    }
+      		})
+	
+});
 
 //$("body").append("<div style='width:50px;height:50px;background:#000;position:absolute;right:0;bottom:0;z-index:1000;' onclick='window.location.reload();'>reload</div><script src='http://10.8.66.213:1337/vorlon.js'></script>");
 
