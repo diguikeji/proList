4a27d74f76dcaa97cfd8e76077ab74e9eec87ffc
guzhenfu <gzfgeh@sina.com>
2018-09-16 01:14:53 +0800

    change files

--- loan/js/app.js
+++ loan/js/app.js
@@ -59,6 +59,18 @@ $(function()
     	$(".mui-content").html('<div class="error-col"><img src="../images/error/xiaoxi.png"/></div>');
 	
     },
+    showModal:function(title, msg){
+    		var html = '<div class="modal-mask row"><div class="modal-dialog"><img src="../images/close_icon.png" class="closeDialg" /><div class="modal-content"><div class="dialog_title">'
+    					+title+'</div><div class="dialog_content">'+msg+
+    					'</div></div></div></div>';
+    		$(document.body).append(html);
+    		
+    		$('.closeDialg').click(function(){
+			console.log("close dialg");
+			$('.modal-mask').addClass("hideClass");
+		});
+
+    },
     commonAjax:function(params, callback)
     {
     		$.ajax({
@@ -109,6 +121,8 @@ $(".mui-content").on("click",".go-home",function()
       		})
 	
 });
+
+
 
 //$("body").append("<div style='width:50px;height:50px;background:#000;position:absolute;right:0;bottom:50px;z-index:1000;' onclick='window.location.reload();'>reload</div><script src='http://192.168.199.203:1337/vorlon.js'></script>");
 
