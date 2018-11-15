eb48aa9849f9fd8b2d64574b8750f0bd353c6dc9
guzhenfu <gzfgeh@sina.com>
2018-11-15 19:05:06 +0800

    fix bug

--- loan/js/home.js
+++ loan/js/home.js
@@ -872,7 +872,7 @@ function clickFindItem(item) {
             method: "POST"
         },
         function(data) {
-            if (mui.os.android && (data.jumpType == "out")) {
+            if (mui.os.android && (item.jumpType == "out")) {
                 plus.runtime.openURL(item.goodsUrl);
                 return;
             }
